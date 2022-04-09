9031a920968c40c3977996e794d6b0ecf2a265a0
mumukers <98086083+mumukers@users.noreply.github.com>
2022-04-09 17:47:31 +0800

    完整版阶段性提交

--- src/JavaseProjects/HouseRent/Server/Server.java
+++ src/JavaseProjects/HouseRent/Server/Server.java
@@ -2,6 +2,8 @@ package JavaseProjects.HouseRent.Server;
 
 import JavaseProjects.HouseRent.domain.House;
 
+import java.net.BindException;
+
 public class Server {
     private static int houseNum=1;
     private static int Id=0;
@@ -9,6 +11,10 @@ public class Server {
     public Server(int size){
         houses=new House[size];
         houses[0]=new House(1,"李四","123456789","123456",2000,"未出租");
+    }
+
+    public House getHouse(int index){
+        return houses[index];
     }
 
     public void list(){
@@ -27,4 +33,36 @@ public class Server {
         return true;
     }
 
+    public int find(int id) {
+        for (int i = 0; i < houseNum; i++) {
+            if (id==houses[i].getId()){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean del(int id) {
+        int index=find(id);
+        if(index!=-1){
+            for (int i = index; i < houseNum - 1; i++) {
+                houses[i]=houses[i+1];
+            }
+            houses[houseNum--]=null;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean update(int id) {
+        int index=find(id);
+        if(index!=-1){
+            for (int i = index; i < houseNum - 1; i++) {
+                houses[i]=houses[i+1];
+            }
+            houses[houseNum--]=null;
+            return true;
+        }
+        return false;
+    }
 }
--- src/JavaseProjects/HouseRent/View/mainMenu.java
+++ src/JavaseProjects/HouseRent/View/mainMenu.java
@@ -31,9 +31,82 @@ public class mainMenu {
         }
 
     }
+    public void finHouse(){
+        System.out.println("============查找房屋============");
+        System.out.print("请输入要查找房屋编号(-1退出)：");
+        int id = Utility.readInt();
+        if(id==-1){
+            System.out.println("============退出查找============");
+            return;
+        }
+        int index=service.find(id);
+        if (index!=-1){
+            System.out.println("编号\t房主\t  电话\t\t  地址\t\t租金\t\t状态");
+            System.out.println(service.getHouse(index));
+            System.out.println("============查找成功============");
+        }else {
+            System.out.println("============未找到房屋============");
+        }
+    }
     public void delHouse(){
+        System.out.println("============删除房屋============");
+        System.out.print("请输入要删除房屋编号(-1退出)：");
+        int id = Utility.readInt();
+        if(id==-1){
+            System.out.println("============退出删除============");
+            return;
+        }
+        if(service.del(id)){
+            System.out.println("============删除成功============");
+            return;
+        }
+            System.out.println("============未找到房屋============");
+    }
+    public  void updateInfo(){
+        System.out.println("============修改房屋信息============");
+        System.out.print("请输入修改房屋编号：(-1退出)：");
+        int id = Utility.readInt();
+        if(id==-1){
+            System.out.println("============退出删除============");
+            return;
+        }
+        int index=service.find(id);
+        if(index==-1){
+            System.out.println("============未找到房屋============");
+            return;
+        }
+        House house= service.getHouse(index);
+        System.out.println("请输入姓名("+house.getName()+")：");
+        String name = Utility.readString(5, house.getName());
+        house.setName(name);
+
+        System.out.println("请输入电话号码("+house.getPhone()+")：");
+        String phone = Utility.readString(5, house.getPhone());
+        house.setPhone(phone);
+
+        System.out.println("请输入地址("+house.getAddress()+")：");
+        String address = Utility.readString(5, house.getAddress());
+        house.setAddress(address);
+
+        System.out.println("请输入租金("+house.getRent()+")：");
+        int rent = Utility.readInt();
+        house.setRent(rent);
+
+        System.out.println("请输入状态("+house.getState()+")：");
+        String state = Utility.readString(5, house.getState());
+        house.setState(state);
 
     }
+    public void exit(){
+        System.out.print("你是否真的要退出(Y/N)：");
+        char choice = Utility.readConfirmSelection();
+        if(choice=='Y'){
+            loop=false;
+            return;
+        }
+        System.out.println("============返回主菜单============");
+    }
+
     public void mainMenu(){
         do{
             System.out.println("1.新增房屋");
@@ -42,7 +115,7 @@ public class mainMenu {
             System.out.println("4.修改房屋信息");
             System.out.println("5.房屋列表");
             System.out.println("6.退出系统");
-            System.out.println("请输入你的选择：");
+            System.out.print("请输入你的选择：");
             char key = Utility.readMenuSelection();
             switch (key){
                 case '1':
@@ -51,12 +124,15 @@ public class mainMenu {
                     break;
                 case '2':
                     //查找房屋
+                    finHouse();
                     break;
                 case '3':
                     //删除房屋
+                    delHouse();
                     break;
                 case '4':
                     //修改房屋信息
+                    updateInfo();
                     break;
                 case '5':
                     //房屋列表
@@ -64,7 +140,7 @@ public class mainMenu {
                     break;
                 case '6':
                     //新增房屋
-                    loop=false;
+                    exit();
                     break;
 
             }
